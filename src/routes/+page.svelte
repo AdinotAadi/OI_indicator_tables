6c6deacfe12dbb0b50ac89cfd0cfb4b553db4dcf
AdinotAadi <adi.notaadi@gmail.com>
2024-01-02 11:48:59 +0530

    feat(tables): Added dynamic tables

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import BankNiftyTable from "../components/BankNiftyTable.svelte";
+	import NiftyTable from "../components/NiftyTable.svelte";
+</script>
+
+<NiftyTable />
+<BankNiftyTable />
