6e2d1e9f5d70a5535eb1bcaf63e0d7d5808c8383
AdinotAadi <adi.notaadi@gmail.com>
2024-01-03 11:27:10 +0530

    Create new API=Params branch

---
+++ src/components/SymbolSelector.svelte
@@ -0,0 +1,21 @@
+<script>
+	let selectedSymbols = [];
+
+	const symbolOptions = [
+		{ name: "NIFTY", id: "symbol1" },
+		{ name: "BANKNIFTY", id: "symbol2" },
+		{ name: "FINNIFTY", id: "symbol3" },
+		{ name: "MIDCPNIFTY", id: "symbol4" },
+		{ name: "SENSEX", id: "symbol5" },
+		{ name: "BANKEX", id: "symbol6" },
+	];
+
+	function generateUrl() {
+		const selectedParams = selectedSymbols.map(
+			(symbol) => `${symbol.id}=${symbol.name}`
+		);
+		const baseUrl =
+			"https://website-backend-ashy.vercel.app/analytics/oi_indicator/";
+		return baseUrl + "?" + selectedParams.join("&");
+	}
+</script>
