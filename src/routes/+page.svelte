9611f3761aee5d01c8a23b7bf6e522cc4c3c63ed
AdinotAadi <adi.notaadi@gmail.com>
2024-01-03 12:43:38 +0530

    feat(api)!: Fetch data from API for selected symbols

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,80 @@
 <script>
-	import { onMount, afterUpdate } from "svelte";
+	import { onMount, afterUpdate, onDestroy } from "svelte";
 	import OITable from "../components/OITable.svelte";
+	import SymbolSelector from "../components/SymbolSelector.svelte";
 
-	let symbolName = "NIFTY";
-	let ceData = [];
-	let peData = [];
+	let apiUrl = "";
+	let tablesData = [];
+
+	const symbolOptions = [
+		"NIFTY",
+		"BANKNIFTY",
+		"FINNIFTY",
+		"MIDCPNIFTY",
+		"SENSEX",
+		"BANKEX",
+	];
+	let checkboxStates = {};
+	symbolOptions.forEach((symbol) => {
+		checkboxStates[symbol] = symbol === "NIFTY";
+	});
+
+	let selectedSymbols = ["NIFTY"];
+
+	function constructApiUrl() {
+		apiUrl = `https://website-backend-ashy.vercel.app/analytics/oi_indicator/?`;
+		selectedSymbols.forEach((symbol, index) => {
+			apiUrl += `symbol${index + 1}=${symbol}&`;
+		});
+		console.log(apiUrl);
+	}
+
+	onMount(constructApiUrl);
+
+	onDestroy(() => {
+		checkboxStates = {};
+	});
+
+	function handleCheckboxChange(event, symbol) {
+		checkboxStates[symbol] = event.target.checked;
+		selectedSymbols = symbolOptions.filter((symbol) => checkboxStates[symbol]);
+		constructApiUrl();
+	}
 
 	async function fetchData() {
-		const response = await fetch("src/data/response.json");
+		const response = await fetch(apiUrl);
 		const data = await response.json();
-		ceData = Object.values(data[symbolName].CE);
-		peData = Object.values(data[symbolName].PE);
+
+		tablesData = selectedSymbols.map((symbol) => {
+			const ceData = data[symbol]?.CE || [];
+			const peData = data[symbol]?.PE || [];
+
+			return {
+				symbolName: symbol,
+				ceData: Object.values(ceData),
+				peData: Object.values(peData),
+			};
+		});
 	}
 
 	onMount(fetchData);
 	afterUpdate(fetchData);
 </script>
 
-<h2>{symbolName} Tables:</h2>
-<OITable {symbolName} {ceData} {peData} />
+<div>
+	{#each symbolOptions as symbol (symbol)}
+		<label>
+			<input
+				type="checkbox"
+				bind:checked={checkboxStates[symbol]}
+				on:change={(e) => handleCheckboxChange(e, symbol)}
+			/>
+			{symbol}
+		</label>
+	{/each}
+</div>
+
+{#each tablesData as { symbolName, ceData, peData }}
+	<h2>{symbolName} Tables:</h2>
+	<OITable {symbolName} {ceData} {peData} />
+{/each}
